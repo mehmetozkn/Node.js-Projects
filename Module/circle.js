c55124c8aeb138ddbad97ac569fe122808511fc3
Mehmet Özkan <75026832+mehmetozkn@users.noreply.github.com>
2022-09-18 20:14:10 +0300

    .

--- Module/circle.js
+++ Module/circle.js
@@ -1,3 +1,12 @@
+/*
+Ödev 3
+Daire Modüler Dosyası
+Daire alan : circleArea ve daire çevre : circleCircumference fonksiyonları içeren ve consola sonuçları yazdıran circle.js dosyası oluşturunuz.
+module.exports yöntemi ile fonksiyonları oluştururken export ediniz.
+require ve object destructing kullanarak index.js dosyasında yarıçap (r) 5 olacak şekilde ekran çıktısını alınız.
+Kolay gelsin.
+*/
+
 const circleArea = (r) => {
   return 3.14 * r * r
 }
